f98ffc8b581f6d0877750abf15afbc3ca2ff634b
Mohit Chaniyal <61289724+mohitchaniyal@users.noreply.github.com>
2023-10-16 15:40:31 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print("Hi Script is runnig")
+import requests
+from bs4 import BeautifulSoup as soup
+from datetime import datetime
+import pandas as pd
+
+master=pd.DataFrame()
+for agency in agencies:
+    for market in markets:
+        resp=requests.get(f"{url}/{market}/{agency}")
+        data=soup(resp.content)
+        df=pd.read_html(str(data.find('table',{"class":"dataTable"})))[0]
+        df["Agency"]=agency
+        df["Market"]=market
+        df["RequestDate"]=datetime.now().date().strftime('%Y-%m-%d')
+        master=pd.concat([master,df],ignore_index=True)
